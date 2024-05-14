1be46a98e23edac79cbf8a86d48b4dfc361eec26
Robert Walling <robertwalling@Roberts-Laptop.local>
2024-05-13 23:36:59 -0400

    Added generic API request handler

--- dunkWatch.js
+++ dunkWatch.js
@@ -25,37 +25,47 @@ class dunkWatchAPI  {
     }
 
     /**
+     * Handles a generic API request
+     * @param {string} endpoint - The API endpoint.
+     * @param {Object} params - The query parameters.
+     * @returns {Promise<Object>} - The API response data.
+     * @throws {Error} - Throws an error if there is an issue with the API request.
+     */
+    async handleAPIRequest(endpoint, params = {}) {
+        try {
+            const response = await this.apiInstance.get(endpoint, { params });
+            return response.data.data;
+        } catch (error) {
+            console.error(`Error fetching data from ${endpoint}:`, error);
+            throw error;
+        }
+    }
+
+    /**
      * Fetches player stats for the given date range
      * 
      * @param {number} [per_page=25] - The number of results to retrieve per page.
      * @param {string} [start_date=currDate] - The start date for the stats retrieval in YYYY-MM-DD format.
      * @param {string} [end_date=currDate] - The end date for the stats retrieval in YYYY-MM-DD format.
      * @returns {Promise<void>} - A promise that resolves when the stats have been fetched and logged.
-     * 
      * @throws {Error} - Throws an error if there is an issue with the API request.
      */
     async fetchStats(per_page=25, start_date=currDate, end_date=currDate) {
-        try {
-            const endpoint = 'stats'
-            const params = { cursor: 0, per_page, start_date, end_date };
+        const endpoint = 'stats'
+        const params = { cursor: 0, per_page, start_date, end_date };
 
-            const response = await this.apiInstance.get(endpoint, { params });
-
-            const stats = response.data.data;
-            
-            if (stats && stats.length > 0) {
-                stats.forEach(stat => {
-                    console.log(`Player: ${stat.player.first_name} ${stat.player.last_name}`);
-                    console.log(`Points: ${stat.pts}`);
-                    console.log(`Rebounds: ${stat.reb}`);
-                    console.log(`Assists: ${stat.ast}`);
-                    console.log('---------------------------');
-                });
-            } else {
-                console.log('No stats available for the given date range.');
-            }
-        } catch (error) {
-            console.error('Error fetching game stats: ', error);
+        const stats = await this.handleAPIRequest(endpoint, params)
+        
+        if (stats && stats.length > 0) {
+            stats.forEach(stat => {
+                console.log(`Player: ${stat.player.first_name} ${stat.player.last_name}`);
+                console.log(`Points: ${stat.pts}`);
+                console.log(`Rebounds: ${stat.reb}`);
+                console.log(`Assists: ${stat.ast}`);
+                console.log('---------------------------');
+            });
+        } else {
+            console.log('No stats available for the given date range.');
         }
     }
     
@@ -66,29 +76,22 @@ class dunkWatchAPI  {
      * @param {string} [start_date=currDate] - The start date for the stats retrieval in YYYY-MM-DD format.
      * @param {string} [end_date=currDate] - The end date for the stats retrieval in YYYY-MM-DD format.
      * @returns {Promise<void>} - A promise that resolves when the stats have been fetched and logged.
-     * 
      * @throws {Error} - Throws an error if there is an issue with the API request.
      */
     async fetchGames(per_page=25, start_date=currDate, end_date=currDate) {
-        try {
-            const endpoint = 'games'
-            const params = { cursor: 0, per_page, start_date, end_date };
-            
-            const response = await this.apiInstance.get(endpoint, { params });
-    
-            const games = response.data.data;
-    
-            if (games && games.length > 0) {
-                games.forEach(game => {
-                    console.log(`Home Team: ${game.home_team.name}`);
-                    console.log(`Away Team: ${game.visitor_team.name}`);
-                    console.log('---------------------------');
-                })
-            } else {
-                console.log('No games available for the given date range.');
-            }
-        } catch (error) {
-            console.error('Error fetching games: ', error);
+        const endpoint = 'games'
+        const params = { cursor: 0, per_page, start_date, end_date };
+
+        const games = await this.handleAPIRequest(endpoint, params)
+
+        if (games && games.length > 0) {
+            games.forEach(game => {
+                console.log(`Home Team: ${game.home_team.name}`);
+                console.log(`Away Team: ${game.visitor_team.name}`);
+                console.log('---------------------------');
+            })
+        } else {
+            console.log('No games available for the given date range.');
         }
     }
     
@@ -96,27 +99,21 @@ class dunkWatchAPI  {
      * Fetches basketball teams and logs their information
      * 
      * @returns {Promise<void>} - A promise that resolves when the teams have been fetched and logged.
-     * 
      * @throws {Error} - Throws an error if there is an issue with the API request.
      */
     async fetchTeams() {
-        try {
-            const endpoint = 'teams';
-            const response = await this.apiInstance.get(endpoint);
+        const endpoint = 'teams';
 
-            const teams = response.data.data;
+        const teams = this.handleAPIRequest(endpoint)
 
-            if (teams && teams.length > 0) {
-                teams.forEach(team => {
-                    console.log(`Team Name: ${team.full_name}`);
-                    console.log(`City: ${team.city}`);
-                    console.log('---------------------------');
-                });
-            } else {
-                console.log('No teams available.');
-            }
-        } catch (error) {
-            console.error('Error fetching teams: ', error);
+        if (teams && teams.length > 0) {
+            teams.forEach(team => {
+                console.log(`Team Name: ${team.full_name}`);
+                console.log(`City: ${team.city}`);
+                console.log('---------------------------');
+            });
+        } else {
+            console.log('No teams available.');
         }
     }
 }
--- main.js
+++ main.js
@@ -3,6 +3,6 @@ const DunkWatchAPI = require("./dunkWatch.js");
 
 const dunkWatchAPI = new DunkWatchAPI(process.env.API_KEY);
 
-dunkWatchAPI.fetchStats(per_page=5);
 dunkWatchAPI.fetchGames(per_page=5);
-dunkWatchAPI.fetchTeams(per_page=5);
+
+dunkWatchAPI.fetchStats(per_page=5);
