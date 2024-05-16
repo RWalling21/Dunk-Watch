240eaaa1e5803d8b23e74024bea2d56863a4a1d3
Rwalling21 <rhw8246@rit.edu>
2024-05-15 23:18:41 -0400

    Base commander setup

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+const { program } = require('commander');
 const DunkWatchAPI = require('./dunkWatch.js');
 
 const dunkWatchAPI = new DunkWatchAPI();
-
 const scoreboard = dunkWatchAPI.fetchScoreboard()
 
-dunkWatchAPI.printScoreboard(scoreboard);
+program
+    .name('dunk-watch')
+    .description('CLI to display current NBA scores')
+    .version('0.0.1')
+    .option('-a, --all', 'Display with all stats')
+    .option('-s, --slim', 'Display only the score and team name')
+    .option('-c, --current', 'Only display current games')
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+if (options.all) dunkWatchAPI.printScoreboard(scoreboard);
+if (options.slim) console.log("Slim Endpoint");
+if (options.current) console.log("Current Endpoint");
+        
